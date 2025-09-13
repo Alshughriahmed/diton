f1bc485fb064c04413cc9f9c5812c9ab9b1ff6fb
Your Name <you@example.com>
2025-09-13 00:18:41 +0000

    fix(ui): make RemoteTopRight a thin wrapper around new FilterBar

--- src/components/chat/RemoteTopRight.tsx
+++ src/components/chat/RemoteTopRight.tsx
@@ -1,16 +1,6 @@
-import FilterBar from "@/app/chat/components/FilterBar";
 "use client";
+import FilterBar from "@/app/chat/components/FilterBar";
 
-
-/**
- * فلاتر أعلى يمين القسم العلوي (منطقة الطرف الثاني)
- * Country/Gender مع z-50 ومنسدلان
- */
 export default function RemoteTopRight() {
-  return (
-    <div data-remote-top-right className="flex gap-2">
-      <CountrySelect />
-      <GenderSelect />
-    </div>
-  );
+  return <FilterBar />;
 }
