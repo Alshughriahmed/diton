9313a38f36f803945e22a4860ad37a39c8050d5a
Alshughriahmed <Alshughriahmed@gmail.com>
2025-10-21 20:33:25 +0200

    Update page.tsx

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,9 @@
-import ChatClient from "./ChatClient";
-
+// server wrapper for chat
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
+
+import dynamic from "next/dynamic";
+const ChatClient = dynamic(() => import("./ChatClient"), { ssr: false });
 
 export default function Page() {
   return <ChatClient />;
